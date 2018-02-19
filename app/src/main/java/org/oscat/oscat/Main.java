b7a6baf5ac51785f44c5faa286c59207bb19d634
Tillson Galloway <tillson.galloway@gmail.com>
2018-02-19 14:52:23 -0500

    Add basic app configuration

--- app/src/main/java/org/oscat/oscat/Main.java
+++ app/src/main/java/org/oscat/oscat/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("No flags specified.  Defaulting to GUI!");
+            System.out.println("~~ gui is not yet implemented.  exiting... ~~");
+            return;
+        }
+
         System.out.println("Hello world!");
 
     }
